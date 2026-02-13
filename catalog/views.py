deab1b46390f243bc8b652f1013edce6add9eb02
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-13 22:35:07 +0300

    added in page home last 5 products

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    most_recent_five_products = Product.objects.order_by("-created_at")[:5]
+    return render(request, "home.html", {"products": most_recent_five_products})
 
 
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
         return HttpResponse(f"{name} сообщение успешно отправлено!!!")
     return render(request, "contacts.html")
