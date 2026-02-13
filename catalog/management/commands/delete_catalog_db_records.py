c6eda10b86d2f5fff3727b3e3581afa8f89bd080
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-13 19:59:38 +0300

    added castom command load_category_fixture

--- catalog/management/commands/delete_catalog_db_records.py
+++ catalog/management/commands/delete_catalog_db_records.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 from catalog.models import Product, Category
 
+
 class Command(BaseCommand):
-    help = 'Delete  datas from db'
+    help = "Delete  datas from db"
 
     def handle(self, *args, **kwargs):
         # Удаляем существующие записи
