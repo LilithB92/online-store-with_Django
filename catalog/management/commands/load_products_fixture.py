580bb540bea7c1b5f83b8c4f932bc91adf5db640
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-13 20:06:34 +0300

    added custom command load_products_fixture

---
+++ catalog/management/commands/load_products_fixture.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = "Load categories from fixture"
+
+    def handle(self, *args, **kwargs):
+        call_command("loaddata", "products_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Successfully loaded data from fixture"))
