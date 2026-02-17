4e86278c878a20ca818f111705a03450d1cbad8b
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-17 19:46:10 +0300

    added pagination for page home(home.html)

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.core.paginator import Paginator
 from django.db import OperationalError, IntegrityError, DataError
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
@@ -9,7 +10,10 @@ from catalog.models import Product, Contact
 # Create your views here.
 def home(request):
     products = Product.objects.order_by("created_at")
-    context = {"products": products}
+    paginator = Paginator(products, 2)  # 10 элементов на страницу
+    page_number = request.GET.get("page")  # Получение номера страницы из URL
+    page_obj = paginator.get_page(page_number)
+    context = {"page_obj": page_obj}
     return render(request, "home.html", context)
 
 
