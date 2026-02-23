c9cf786adf05e3a5745764ae28ce9493c5f63884
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-23 13:11:16 +0300

    validation was  added  for price field in Product

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
 
@@ -10,25 +12,32 @@ class ProductForm(forms.ModelForm):
             "updated_at",
         ]
 
+    forbidden_words = [
+        "казино",
+        "криптовалюта",
+        "крипта",
+        "биржа",
+        "дешево",
+        "бесплатно",
+        "обман",
+        "полиция",
+        "радар",
+    ]
+
     def clean(self):
         # Список запрещенных слов
-        forbidden_words = [
-            "казино",
-            "криптовалюта",
-            "крипта",
-            "биржа",
-            "дешево",
-            "бесплатно",
-            "обман",
-            "полиция",
-            "радар",
-        ]
         clean_data = super().clean()
         name = clean_data.get("name")
         description = clean_data.get("description")
-        for forbidden_word in forbidden_words:
+        for forbidden_word in self.forbidden_words:
             if forbidden_word == name.lower():
                 self.add_error("name", f"Имя не может содержать слово {forbidden_word}")
             elif forbidden_word in description.lower():
                 self.add_error("description", f"Описание не может содержать слово {forbidden_word}")
         return clean_data
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price < 0:
+            raise ValidationError('Цена продукта не может быть отрицательной')
+        return price
