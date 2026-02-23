f71e5374412fabdb8f0bca16c3aafc4e5424e05f
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-23 14:07:21 +0300

    validation was  added  for img  field in Product

--- catalog/forms.py
+++ catalog/forms.py
@@ -41,3 +41,13 @@ class ProductForm(forms.ModelForm):
         if price < 0:
             raise ValidationError('Цена продукта не может быть отрицательной')
         return price
+
+    def clean_img(self):
+        img = self.cleaned_data.get('img')
+        max_size_mb = 5
+        extensions = (".png", ".jpg")
+        if img and (img.size > (max_size_mb * 1024 * 1024)):
+            raise ValidationError(f"Максимальный размер файла - {max_size_mb} МБ.")
+        if img and (not img.name.endswith(extensions)):
+            raise ValidationError('Неподдерживаемые форматы изображений. Допускаются только JPEG и PNG.')
+        return img
