c13a4c373b454329a13ddfee27a7d7e975daacd8
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-28 16:07:59 +0300

    added CustemUser model for users with email, phone_number, avatar and country fields

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+AUTH_USER_MODEL = "users.CustomUser"
 
 
 # Database
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True)
+    country = models.CharField(max_length=60, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
