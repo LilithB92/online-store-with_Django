576acf8370806e6ffa8bcc75762f5b60c210b7b8
Lilith <bichakhchyanlilith1992@gmail.com>
2026-02-28 21:49:19 +0300

    added urls for registration

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .apps import UsersConfig
 
+from .apps import UsersConfig
+from .views import RegisterView
 
 app_name = UsersConfig.name
 
 
-
 urlpatterns = [
-    # path('', BlogsListView.as_view(), name='blogs_list'),
-    # path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-    # path('new/', BlogCreateView.as_view(), name='blog_create'),
-
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="catalog:products_list"), name="logout"),
 ]
