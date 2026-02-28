939636cb2f5981558e82412f7c4d27e8480f335d
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-01 02:10:00 +0300

    fixes some bugs of logins and register

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,10 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=15, required=True, help_text="Введите Ваш номер телефона")
+    username = forms.CharField(max_length=50, required=True)
+    usable_password = None
+
     class Meta:
         model = CustomUser
         fields = [
@@ -24,11 +28,11 @@ class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
+            field.help_text = False
             if isinstance(field, BooleanField):
                 field.widget.attrs["class"] = "form-check-input"
             else:
                 field.widget.attrs["class"] = "form-control"
-                field.widget.attrs["placeholder"] = field.help_text
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get("phone_number")
@@ -40,10 +44,9 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = CustomUser
-        fields = ["email", "password"]
+        fields = ["", "email", "password"]
 
     def __init__(self, *args, **kwargs):
         super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
-            field.widget.attrs["placeholder"] = field.help_text
