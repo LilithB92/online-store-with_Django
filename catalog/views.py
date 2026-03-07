5b092b2dbe8e29e837b4057cbe04220e0eb42b91
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-07 18:51:54 +0300

    added functionality that only owner can update product

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from catalog.forms import ProductForm
 from catalog.models import Product, Contact
@@ -30,10 +30,14 @@ class ProductCreateView(LoginRequiredMixin,CreateView):
         return super().form_valid(form)
 
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:products_list")
+
+    def test_func(self):
+        product = self.get_object()
+        return product.owner == self.request.user
 
 
 
