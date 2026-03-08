c33fade28445f50ead78a06650c7a34663ed7fda
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-09 00:04:48 +0300

    added functionality that only with permission can_anpublish_product allow change status

--- catalog/forms.py
+++ catalog/forms.py
@@ -5,15 +5,18 @@ from django.db.models.fields import BooleanField
 from .models import Product
 
 
+class ProductModeratorForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "status",
+        ]
+
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        exclude = [
-            "created_at",
-            "updated_at",
-            'status',
-            'owner'
-        ]
+        exclude = ["created_at", "updated_at", "status", "owner"]
 
     forbidden_words = [
         "казино",
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
@@ -8,6 +8,7 @@ from django.views.generic import ListView
 from django.views.generic import UpdateView
 
 from catalog.forms import ProductForm
+from catalog.forms import ProductModeratorForm
 from catalog.models import Contact
 from catalog.models import Product
 
@@ -35,14 +36,22 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy("catalog:products_list")
 
-    def test_func(self):
-        product = self.get_object()
-        return product.owner == self.request.user
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner:
+            print(user.has_perm("catalog.can_unpublish_product"))
+            return ProductForm
+        elif user.has_perm(
+            "catalog.can_unpublish_product"
+        ):  # elif user.groups.filter(name="Модератор продуктов").exists():
+            return ProductModeratorForm
+        else:
+            return HttpResponseForbidden
 
 
 class ProductDeleteView(DeleteView):
