df2ba8ac9866fc03b6b898bb45e83ee067ef0827
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-15 10:04:18 +0300

    added package redis and sat cache

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,12 @@ STATICFILES_DIRS = [
 
 MEDIA_URL ='media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+        }
+    }
