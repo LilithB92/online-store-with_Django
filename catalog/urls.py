76985103bb4232afc4b3f343fe77b386808a8d9f
Lilith <bichakhchyanlilith1992@gmail.com>
2026-03-15 10:52:17 +0300

    sat cache_page for product_detail page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import ContactCreateView
@@ -13,7 +14,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("products/", ProductList.as_view(), name="products_list"),
     path("contacts/new/", ContactCreateView.as_view(), name="contact_create"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_details"),
+    path("product/<int:pk>/", cache_page(60 * 15)(ProductDetailView.as_view()), name="product_details"),
     path("product/new/", ProductCreateView.as_view(), name="product_create"),
     path("product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
     path("product/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
